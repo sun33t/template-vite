61596e98f6d888109dece6e55867343ad9e3d479
Suneet Misra <hello@suneet.codes>
2023-04-09 00:37:25 +0100

    refactor: encapsulates all routing markup within routes directory

--- src/main.tsx
+++ src/main.tsx
@@ -1,37 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { Layout } from '~components/Layout';
-import { About } from '~routes/About';
-import { Blog } from '~routes/Blog';
-import { Contact } from '~routes/Contact';
-import { Home } from '~routes/Home';
+import { RouterProvider } from 'react-router-dom';
+import { router } from '~routes/index';
 import './index.css';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: '/contact',
-        element: <Contact />,
-      },
-      {
-        path: '/about',
-        element: <About />,
-      },
-      {
-        path: '/blog',
-        element: <Blog />,
-      },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
