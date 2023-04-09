35f41e99ccbb82e3d1eab56ad592d1b50c8fc4d2
Suneet Misra <hello@suneet.codes>
2023-04-09 08:26:25 +0100

    setup: moves postcss.config.js file to .config dir

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: '.config/',
+  },
   resolve: {
     alias: {
       '~ui': path.resolve(__dirname, 'src/components/ui'),
